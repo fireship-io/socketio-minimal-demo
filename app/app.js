e1ffb14a635b4b38d1b5b44edfda8b5dad61b389
juanc-tiv <juanc@tiv.solutions>
2022-08-26 13:31:47 -0400

    added express usage sample

--- app/app.js
+++ app/app.js
@@ -1,20 +1,15 @@
+const socket = io("ws://localhost:8080");
 
-const socket = io('ws://localhost:8080');
-
-socket.on('message', text => {
-
-    const el = document.createElement('li');
-    el.innerHTML = text;
-    document.querySelector('ul').appendChild(el)
-
+socket.on("message", (text) => {
+  const el = document.createElement("li");
+  el.innerHTML = text;
+  document.querySelector("ul").appendChild(el);
 });
 
-document.querySelector('button').onclick = () => {
-
-    const text = document.querySelector('input').value;
-    socket.emit('message', text)
-    
-}
+document.querySelector("button").onclick = () => {
+  const text = document.querySelector("input").value;
+  socket.emit("message", text);
+};
 
 // Regular Websockets
 
--- server/index.js
+++ server/index.js
@@ -1,28 +1,63 @@
+const express = require("express");
+const socket = require("socket.io");
 
-const http = require('http').createServer();
+// App setup
+const PORT = 8080;
+const app = express();
 
-const io = require('socket.io')(http, {
-    cors: { origin: "*" }
+// sample route
+app.get("/user", (req, res) => {
+    res.send("<h1>Hello World</h1>");
+})
+
+const server = app.listen(PORT, function () {
+  console.log(`Listening on port ${PORT}`);
 });
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
-
-    socket.on('message', (message) =>     {
-        console.log(message);
-        io.emit('message', `${socket.id.substr(0,2)} said ${message}` );   
-    });
+// Socket setup
+const io = socket(server, {
+  cors: { origin: "*" },
 });
 
-http.listen(8080, () => console.log('listening on http://localhost:8080') );
+io.on("connection", function (socket) {
+  console.log("a user connected");
+  socket.on("message", (message) => {
+    console.log(message);
+    io.emit("message", `${socket.id.substr(0, 2)} said ${message}`);
+  });
+});
 
 
-// Regular Websockets
+
+// ------------------------------------------------------------------------------
+// REGULAR HTTP SERVER
+
+// const http = require('http').createServer();
+
+// const io = require('socket.io')(http, {
+//     cors: { origin: "*" }
+// });
+
+// io.on('connection', (socket) => {
+//     console.log('a user connected');
+
+//     socket.on('message', (message) =>     {
+//         console.log(message);
+//         io.emit('message', `${socket.id.substr(0,2)} said ${message}` );
+//     });
+// });
+
+// http.listen(8080, () => console.log('listening on http://localhost:8080') );
+
+
+
+// --------------------------------------------------------------------------------
+// REGULAR WEBSOCKET
 
 // const WebSocket = require('ws')
 // const server = new WebSocket.Server({ port: '8080' })
 
-// server.on('connection', socket => { 
+// server.on('connection', socket => {
 
 //   socket.on('message', message => {
 
@@ -31,6 +66,3 @@ http.listen(8080, () => console.log('listening on http://localhost:8080') );
 //   });
 
 // });
-
-
- 
