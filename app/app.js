5a63dfb52a385d42c8988dece375c20c5bf1c822
Michael Truong <truong.minh.michael@gmail.com>
2023-05-23 12:28:40 -0700

    made it so you can see chat history when first connecting

--- app/app.js
+++ app/app.js
@@ -1,20 +1,26 @@
 
-const socket = io('ws://localhost:8080');
+const socket = io('http://localhost:8080');
 
 socket.on('message', text => {
+  const el = document.createElement('li');
+  el.innerHTML = text;
+  document.querySelector('ul').appendChild(el);
+});
 
+socket.on('chatHistory', chatHistory => {
+  const ul = document.querySelector('ul');
+  chatHistory.forEach(message => {
     const el = document.createElement('li');
-    el.innerHTML = text;
-    document.querySelector('ul').appendChild(el)
-
+    el.innerHTML = message;
+    ul.appendChild(el);
+  });
 });
 
 document.querySelector('button').onclick = () => {
+  const text = document.querySelector('input').value;
+  socket.emit('message', text);
+};
 
-    const text = document.querySelector('input').value;
-    socket.emit('message', text)
-    
-}
 
 // Regular Websockets
 
--- server/index.js
+++ server/index.js
@@ -1,20 +1,26 @@
-
 const http = require('http').createServer();
-
 const io = require('socket.io')(http, {
-    cors: { origin: "*" }
+  cors: { origin: "*" }
 });
+
+const chatMessages = []; // Collection to store chat messages
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
+  console.log('a user connected');
 
-    socket.on('message', (message) =>     {
-        console.log(message);
-        io.emit('message', `${socket.id.substr(0,2)} said ${message}` );   
-    });
+  // Send previous chat messages to the newly connected client
+  socket.emit('chatHistory', chatMessages);
+
+  socket.on('message', (message) => {
+    console.log(message);
+    const chatMessage = `${socket.id.substr(0, 2)} said ${message}`;
+    chatMessages.push(chatMessage); // Store the new chat message
+    io.emit('message', chatMessage);
+  });
 });
 
-http.listen(8080, () => console.log('listening on http://localhost:8080') );
+http.listen(8080, () => console.log('listening on http://localhost:8080'));
+
 
 
 // Regular Websockets
